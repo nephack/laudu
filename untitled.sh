31a298891c77a55af2a31db57cbb2bf9f8397342
nephack <105436661+nephack@users.noreply.github.com>
2024-03-25 11:18:03 -0400

    Add files via upload

---
+++ untitled.sh
@@ -0,0 +1,4 @@
+sudo apt install default-jdk
+wget https://dl2.tlauncher.org/f.php?f=files%2FTLauncher-2.899.zip
+unzip 'f.php?f=files%2FTLauncher-2.899.zip'
+sudo cp tlauncher.desktop /usr/share/applications
